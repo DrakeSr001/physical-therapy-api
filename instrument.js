983c392505e9d4085c7170a41bc8cb339c7b00ab
DrakeSr001 <magedalicf@gmail.com>
2025-11-11 17:30:54 +0200

    Update V3.6.1 Sentry

--- instrument.js
+++ instrument.js
@@ -5,5 +5,6 @@ Sentry.init({
   dsn: "https://8a67d9c7dcc9f5f52e305979271e6ee2@o4510347058085888.ingest.us.sentry.io/4510347058348032",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
+  //
   sendDefaultPii: true,
 });
